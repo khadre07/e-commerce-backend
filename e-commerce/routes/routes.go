e39235581a1f559eaac32f845a04367cd7794735
khadre07 <99775997+khadre07@users.noreply.github.com>
2024-11-08 14:43:29 -0500

    backend ecommerce updated

---
+++ e-commerce/routes/routes.go
@@ -0,0 +1,17 @@
+package routes
+
+import (
+	"commerce/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func UserRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/signup", controllers.SignUp())
+	incomingRoutes.POST("/users/login", controllers.Login())
+	incomingRoutes.POST("/admin/addproduct", controllers.ProductViewerAdmin())
+	incomingRoutes.GET("/users/productview", controllers.SearchProduct())
+	incomingRoutes.GET("users/search", controllers.SearchProductByQueryByName())
+	incomingRoutes.GET("users/searchno", controllers.SearchProductByQueryByNo())
+
+}
